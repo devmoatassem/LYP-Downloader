5e6dee7120f1e9b51e8bdec04d076557e105b4f8
MOATASSEM BILLAH SINDHU <moatassemsindhu@gmail.com>
2021-09-12 18:25:47 +0500

    V 1.1
    Downloading support for YouTube Videos has been added

--- index.py
+++ index.py
@@ -26,6 +26,13 @@ class MainApp(QMainWindow , ui):
     def BUtton_Hnadler(self):
         self.pushButton.clicked.connect(self.Download)
         self.pushButton_2.clicked.connect(self.Browse_Location)
+<<<<<<< Updated upstream
+=======
+        ##Get video information in Single youtube video
+        self.pushButton_7.clicked.connect(self.Video_data)
+        self.pushButton_14.clicked.connect(self.Download_Video)
+        self.pushButton_4.clicked.connect(self.Save_location_video)
+>>>>>>> Stashed changes
         
     ##Calculate the progress
     def Progress_Handler(self,blocknum,blocksize,totalsize):
@@ -40,7 +47,7 @@ class MainApp(QMainWindow , ui):
     ##Browsing to file explorer
     def Browse_Location(self):
         save_location=QFileDialog().getSaveFileName(self, caption="Save As", directory=".", filter="All Files(*.*)")
-        print(save_location)
+        
         
         self.lineEdit_2.setText(str(save_location[0]))
     
@@ -48,7 +55,7 @@ class MainApp(QMainWindow , ui):
     
     ##Downloading files
     def Download(self):
-        print('Starting Download')
+        
         
         download_url=self.lineEdit.text()
         Save_location=self.lineEdit_2.text()
@@ -79,11 +86,80 @@ class MainApp(QMainWindow , ui):
             
          
                 
+<<<<<<< Updated upstream
     ##Save Location in Line edit
     def Save_location(self):
         pass
 
 
+=======
+   
+ 
+    #############################################
+    ################ YOUTUBE#####################
+    #############################################
+    def Video_data(self):
+        ##storing entered text in variaable
+        video_url=self.lineEdit_4.text()
+        
+        if video_url=='':
+            QMessageBox.information(self ,"Data Error" , "PLease provide the valid URL") 
+        else:
+            video=pafy.new(video_url)
+            
+    
+            video_stream=video.videostreams
+            for stream in video_stream:
+                size=humanize.naturalsize(stream.get_filesize())
+                data="{} {} {} {}".format(stream.mediatype,stream.extension,stream.quality ,size )
+                
+                ##giving data to box for quality select
+                self.comboBox.addItem(data)
+    ##Save Location in Line edit
+    def Save_location_video(self):
+        save_location=QFileDialog().getSaveFileName(self, caption="Save As", directory=".", filter="All Files(*.*)")
+
+        self.lineEdit_3.setText(str(save_location[0]))           
+    
+    ##Download Single Youtube video            
+    def Download_Video(self):
+        video_url=self.lineEdit_4.text()
+        save_location=self.lineEdit_3.text()
+        
+        
+        if video_url=='' or save_location=='':
+            QMessageBox.warning(self, "Data Error" , "Dear User Please Provide a Valid URL or Save Location")
+        else:
+            video=pafy.new(video_url) 
+            video_stream=video.videostreams
+            video_quality=self.comboBox.currentIndex()
+            download=video_stream[video_quality].download(filepath=save_location , callback=self.Video_progress)
+            
+            ##Check if URL is valid then proceed otherwise stop
+                    
+          
+        ##Download compelete Dialog box 
+        QMessageBox.information(self ,"Download Completed" , "Video Downloaded Successfully") 
+        
+        
+        ##St lines of URL and save location and progress bar to empty after downloading
+        
+        self.lineEdit_4.setText('')
+        self.lineEdit_3.setText('')
+        self.progressBar_2.setValue(0)
+        
+        
+    def Video_progress(self,total,received,ratio,rate,time):
+         read_data=received
+         if total>0:
+             download_percentage=read_data*100/total
+             self.progressBar_2.setValue(download_percentage)
+             remaining_time=round(time/60,2)
+             
+             self.label.setText(str('{} minutes remaining'.format(remaining_time)))
+             QApplication.processEvents()
+        
+>>>>>>> Stashed changes
 def main():
     app=QApplication(sys.argv)
     window=MainApp()
